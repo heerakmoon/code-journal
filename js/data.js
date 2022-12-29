38ec4c3854bbce178b24b233b35019111be105e1
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-29 18:30:02 +0000

    conditional statement checking to see if there is a JSON data model in localStorage and parses it and assigns it to the initial data variable so user can continue adding entries to preexisting data model

--- js/data.js
+++ js/data.js
@@ -11,3 +11,9 @@ var dataJSON = JSON.stringify(data);
 localStorage.setItem('code-journal-local-storage', dataJSON);
 
 window.addEventListener('beforeunload', event);
+
+var existingDataJSON = localStorage.getItem('code-journal-local-storage');
+
+if (existingDataJSON !== null) {
+  data = JSON.parse(existingDataJSON);
+}
